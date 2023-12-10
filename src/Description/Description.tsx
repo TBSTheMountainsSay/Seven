263a1ec70cc7e91f9f3c682df0bb53909b051f2a
TBSTheMountainsSay <blackswadow@yandex.ru>
2023-12-11 01:03:52 +0400

    donate

--- src/Description/Description.tsx
+++ src/Description/Description.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './Description.module.scss';
+import donate from "src/assets/donate.png";
 
 type TDescriptionProps = {
   title: string;
   text: string;
+  img?: boolean;
 };
 
-const Description: React.FC<TDescriptionProps> = ({ title, text }) => {
+const Description: React.FC<TDescriptionProps> = ({ title, text, img }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>{title}</div>
       <div className={styles.text}>{text}</div>
+      {img?<img className={styles.img} src={donate}></img>:<div></div>}
     </div>
   );
 };
